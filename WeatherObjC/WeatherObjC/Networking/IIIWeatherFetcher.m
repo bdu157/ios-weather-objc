87acea3ab27f3e82b91c17041ed5f9ef7ab728a0
Dongwoo Pae <dongwoopae@gmail.com>
2019-11-04 20:02:44 +0900

    dummyString for the json data gets fetched correctly

--- WeatherObjC/WeatherObjC/Networking/IIIWeatherFetcher.m
+++ WeatherObjC/WeatherObjC/Networking/IIIWeatherFetcher.m
@@ -13,7 +13,6 @@ static NSString *const WeatherFetcherBaseURLString = @"https://api.openweatherma
 
 @interface IIIWeatherFetcher ()
 
-
 @end
 
 @implementation IIIWeatherFetcher
@@ -22,7 +21,24 @@ static NSString *const WeatherFetcherBaseURLString = @"https://api.openweatherma
 {
     NSLog(@"Fetching weathersssssss");
     
+    NSURLComponents *URLComponents = [[NSURLComponents alloc] initWithString:WeatherFetcherBaseURLString];
+    
+    NSMutableArray *queryItems = [NSMutableArray arrayWithObjects:
+                                  [NSURLQueryItem queryItemWithName:@"zip" value:@"94040"],
+                                  [NSURLQueryItem queryItemWithName:@"cnt" value:@"7"],
+                                  [NSURLQueryItem queryItemWithName:@"appid" value:@"f144c855a01a9bd80c692dd246ed42e9"],nil];
 
+    URLComponents.queryItems = queryItems;
+    NSURL *URL = URLComponents.URL;
+    
+    //URLSession
+    [[NSURLSession.sharedSession dataTaskWithURL:URL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSLog(@"Finished fetching weathers.....");
+        
+        NSString *dummyString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        
+        NSLog(@"Dummy string: %@", dummyString);
+    }] resume];
 }
 
 @end
